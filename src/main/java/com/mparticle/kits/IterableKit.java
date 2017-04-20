2c74a1f009c3286b28310aed2493d990593d9a34
David Truong <dt@iterable.com>
2017-04-20 12:29:18 -0700

    Updated read and misc changes.

--- src/main/java/com/mparticle/kits/IterableKit.java
+++ src/main/java/com/mparticle/kits/IterableKit.java
@@ -2,7 +2,6 @@ package com.mparticle.kits;
 
 import android.app.Activity;
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 
 import com.iterable.iterableapi.IterableApi;
