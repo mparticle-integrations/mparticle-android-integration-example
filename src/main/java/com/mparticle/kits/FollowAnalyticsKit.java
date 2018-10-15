1169585b0936fc65a52ad9f84c8f6d42704a1264
José Carlos Joaquim <j.joaquim@gmail.com>
2018-10-15 17:32:43 +0200

    Event listener

--- src/main/java/com/mparticle/kits/FollowAnalyticsKit.java
+++ src/main/java/com/mparticle/kits/FollowAnalyticsKit.java
@@ -4,21 +4,16 @@ import android.content.Context;
 
 import java.util.List;
 import java.util.Map;
+import java.util.LinkedList;
 
-public class FollowAnalyticsKit extends KitIntegration implements
-    KitIntegration.EventListenter,
-    KitIntegration.AttributeListener {
+import com.mparticle.MPEvent;
+import com.mparticle.MParticle;
+import com.followanalytics.FollowAnalytics;
+
+public class FollowAnalyticsKit extends KitIntegration implements KitIntegration.EventListener {
 
     @Override
     protected List<ReportingMessage> onKitCreate(Map<String, String> settings, Context context) {
-        /** TODO: Initialize your SDK here
-         * This method is analogous to Application#onCreate, and will be called once per app execution.
-         *
-         * If for some reason you can't start your SDK (such as settings are not present), you *must* throw an Exception
-         *
-         * If you forward any events on startup that are analagous to any mParticle messages types, return them here
-         * as ReportingMessage objects. Otherwise, return null.
-         */
         return null;
     }
 
@@ -37,4 +32,38 @@ public class FollowAnalyticsKit extends KitIntegration implements
         ReportingMessage optOutMessage = new ReportingMessage(this, ReportingMessage.MessageType.OPT_OUT, System.currentTimeMillis(), null);
         return null;
     }
+
+    @Override
+    public List<ReportingMessage> logScreen(String screenName, Map<String, String> eventAttributes) {
+        return null;
+    }
+
+    @Override
+    public List<ReportingMessage> logEvent(MPEvent event) {
+        if (event.getInfo() == null) {
+            FollowAnalytics.logEvent(event.getEventName());
+        }else{
+            FollowAnalytics.logEvent(event.getEventName(), event.getInfo());
+        }
+        List<ReportingMessage> messageList = new LinkedList<ReportingMessage>();
+        messageList.add(ReportingMessage.fromEvent(this,event));
+        return messageList;
+    }
+
+    @Override
+    public List<ReportingMessage> logError(String message, Map<String, String> errorAttributes) {
+        FollowAnalytics.logError(message, errorAttributes);
+        return null;
+    }
+
+    @Override
+    public List<ReportingMessage> logException(Exception exception, Map<String, String> exceptionAttributes, String message) {
+        return null;
+    }
+
+    @Override
+    public List<ReportingMessage> leaveBreadcrumb(String breadcrumb) {
+        return null;
+    }
+
 }
